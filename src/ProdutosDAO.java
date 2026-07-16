2e940564f52f7a78f3031b0a4239a8fb5dc4b816
jesse-barbosa <barbosajesse419@gmail.com>
2026-07-15 22:45:53 -0300

    Implementa salvamento de produtos no banco de dados

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -10,8 +10,8 @@
 
 import java.sql.PreparedStatement;
 import java.sql.Connection;
-import javax.swing.JOptionPane;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 
@@ -22,12 +22,25 @@ public class ProdutosDAO {
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
-    public void cadastrarProduto (ProdutosDTO produto){
-        
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
+    public boolean cadastrarProduto (ProdutosDTO produto){
+        String sql = "INSERT INTO produtos (nome, valor, status) VALUES (?, ?, ?)";
+        conn = new conectaDAO().connectDB();
+
+        if (conn == null) {
+            return false;
+        }
+
+        try (Connection connection = conn;
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setString(1, produto.getNome());
+            statement.setInt(2, produto.getValor());
+            statement.setString(3, produto.getStatus());
+
+            return statement.executeUpdate() == 1;
+        } catch (SQLException erro) {
+            System.err.println("Erro ao cadastrar produto: " + erro.getMessage());
+            return false;
+        }
     }
     
     public ArrayList<ProdutosDTO> listarProdutos(){
